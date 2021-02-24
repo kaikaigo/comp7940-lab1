7ca9c9a20c7742e48dc8390ea64948cbf6fd786a
张杨凯 <zhangyangkai@zhangyangkaideMacBook.local>
2021-02-24 13:41:34 +0800

    hello function

--- chatbot.py
+++ chatbot.py
@@ -30,7 +30,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
+    dispatcher.add_handler(CommandHandler("hello", hello))
 
     # To start the bot:
     updater.start_polling()
@@ -50,6 +50,16 @@ def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Helping you helping you.')
 
+def hello(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
+    try: 
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]   # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('Good day, ' + msg + "!"  )
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /hello <keyword>')
 
 def add(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /add is issued."""
